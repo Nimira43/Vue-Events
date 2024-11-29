d13bc718d17d125121928df5eb84bbf0383dc4f9
Nimira43 <n.rabone72@aol.com>
2024-11-29 06:54:48 +0000

    Tailwind and Antd working well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from 'antd'
+
 export default function page() {
   return (
-    <div>
-      <h1>Stour End Motors</h1>
+    <div className='p-5 flex flex-col gap-5'>
+      <h1 className='text-[#028ec6]'>Stour End Motors</h1>
+      <Button
+        type='primary'
+      >Button</Button>
     </div>
   )
 }
