68493bd3e4e85c5e405a937f3b966efd20733204
Nimira43 <n.rabone72@aol.com>
2024-11-29 21:36:14 +0000

    Brought Tailwind in

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Xeus</h1>
+      <h1 className='bg-[#111] p-5 text-3xl text-[#ff4500]'>Xeus</h1>      
     </div>
   )
 }
 
 export default App
+
