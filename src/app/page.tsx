18d7057eae032f2831ea37a4e7365a86b3122412
Nimira43 <n.rabone72@aol.com>
2024-11-29 07:01:07 +0000

    Designing very simple form to test out Tailwind and Antd together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import { Button } from 'antd'
+import { Button, Input } from 'antd'
 
 export default function page() {
   return (
     <div className='p-5 flex flex-col gap-5'>
-      <h1 className='text-[#028ec6]'>Stour End Motors</h1>
+      <h1 className='text-[#028ec6] text-4xl'>Stour End Motors</h1>
+      <h4 className='text-2xl'>Enter Vehicle Details</h4>
+      <Input 
+      placeholder='Enter Car Model'
+      />
+      <Input 
+      placeholder='Enter Car Registration'
+      />
       <Button
         type='primary'
       >Confirm</Button>
-      
       <Button
         type='default'
       >Cancel</Button>
+
     </div>
   )
 }
