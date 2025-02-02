3118b73e399ed3ba8e0d9d017b4916c42decb0f8
Nimira43 <n.rabone72@aol.com>
2025-02-02 19:11:51 +0000

    Brought in EventCard into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl logo text-prime">Vue Events</h1>
+    <EventCard />
   </main>
 </template>
+
+
+<script setup>
+  import EventCard from '@/components/EventCard.vue'
+</script>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,3 +1,3 @@
 <template>
-  EventCard
+  <div>EventCard</div>
 </template>
