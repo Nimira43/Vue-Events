ce83a3305ffaeaa9c2003038ef051df13c8d6d09
Nimira43 <n.rabone72@aol.com>
2025-02-02 19:28:02 +0000

    Using v-for on EventCard in App.vue to render 8 cards

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-6xl logo text-prime">Vue Events</h1>
     <h2 class="text-2xl font-medium uppercase">All Events</h2>
-    <EventCard />
+    <EventCard v-for="i in 8 " :key="i" />
     <h2 class="text-2xl font-medium  uppercase">Your Bookings</h2>
   </main>
 </template>
