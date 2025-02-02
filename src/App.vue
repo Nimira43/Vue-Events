f817b884416306d7f9f0bd196c7a9092e82b38ff
Nimira43 <n.rabone72@aol.com>
2025-02-02 19:32:49 +0000

    Wrapped th EventCard in a section element and applied grid styles. The EventCard layout is much cleaned.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <main class="container mx-auto my-8 space-y-8">
-    <h1 class="text-6xl logo text-prime">Vue Events</h1>
+    <h1 class="text-4xl logo text-prime">Vue Events</h1>
     <h2 class="text-2xl font-medium uppercase">All Events</h2>
-    <EventCard v-for="i in 8 " :key="i" />
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard v-for="i in 8 " :key="i" />
+    </section>
     <h2 class="text-2xl font-medium  uppercase">Your Bookings</h2>
   </main>
 </template>
