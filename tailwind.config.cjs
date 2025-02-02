94b4a2f3a7041496c8c23780007d41ad0c3c9c9b
Nimira43 <n.rabone72@aol.com>
2025-02-02 18:48:36 +0000

    Setup theme colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#111',
+        'light': '#f5fffa',
+        'prime': '#ff4500',
+        'prime-light': '#fc7b4c',
+        'grey-extra-light': '#eee',
+        'grey-light': '#ccc',
+        'grey-medium': '#777',
+        'grey-dark': '#333',
+      },
+    },
   },
   plugins: [],
 }
