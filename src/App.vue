51dd10b2cc3667ae499920777d878ae816e07a8c
Nimira43 <n.rabone72@aol.com>
2025-02-02 19:16:14 +0000

    Adding extra headings to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl logo text-prime">Vue Events</h1>
+    <h2 class="font-normal uppercase">All Events</h2>
     <EventCard />
+    <h2 class="font-normal uppercase">Your Bookings</h2>
   </main>
 </template>
-
 
 <script setup>
   import EventCard from '@/components/EventCard.vue'
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>EventCard</div>
+  <div>
+    
+  </div>
 </template>
