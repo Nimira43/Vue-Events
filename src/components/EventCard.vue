0792f92fe469bb0c525b2595fb8b5ff6c8197eeb
Nimira43 <n.rabone72@aol.com>
2025-02-02 19:26:20 +0000

    Finishing up styling the EventCard for now.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border border-prime-light bg-grey-extra-light rounded-md ">
     <h3 class="p-4 text-prime uppercase font-medium text-xl">Event Name</h3>
-    <p>Date</p>
-    <p>Description</p>
+    <p class="p-4 text-dark font-normal uppercase">Date</p>
+    <p class="p-4 text-dark font-normal uppercase">Description</p>
   </div>
 </template>
