88a2f83909ec693b43d520bd71edda8fbfae16a6
Nimira43 <n.rabone72@aol.com>
2025-02-23 02:28:30 +0000

    Starting the process of creating a new Button component.

---
+++ src/components/Button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button 
+    class="bg-prime text-light py-2 px-3 rounded-md hover:bg-prime-light cursor-pointer text-sm uppercase"
+    @click="$emit('click me')"
+  >
+    Register
+  </button>
+</template>
