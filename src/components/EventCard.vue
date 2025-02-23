280b546c439be2062e2dd1b80de91c0d6c912cd6
Nimira43 <n.rabone72@aol.com>
2025-02-23 02:09:05 +0000

    Added Register button to EventCard component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,6 +3,12 @@
     <h3 class="p-4 text-prime uppercase font-medium text-xl">{{ eventName }}</h3>
     <p class="p-4 text-dark font-normal uppercase">{{ date }}</p>
     <p class="p-4 text-dark font-normal uppercase">{{ description }}</p>
+    <section class="flex justify-end p-4">
+      <button class="bg-prime text-light py-2 px-4 rounded-md hover:bg-prime-light cursor-pointer">
+        Register
+      </button>
+
+    </section>
   </div>
 </template>
 
@@ -12,5 +18,7 @@ defineProps({
   date: String,
   description: String
 })
+
+defineEmits(['register'])
 
 </script>
