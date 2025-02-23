ef60d6f347f59b662f5ea017056ec06081eb39a3
Nimira43 <n.rabone72@aol.com>
2025-02-23 01:59:53 +0000

    Providing some dummy data  via the props inside the EventCard

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <h1 class="text-4xl logo text-prime">Vue Events</h1>
     <h2 class="text-2xl font-medium uppercase">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8 " :key="i" />
+      <EventCard 
+        v-for="i in 8 " 
+        :key="i" 
+        eventName="Coding Conference 2025"
+        date="4th April 2025"
+        description="Latest developments in the world of Frameworks"  
+      />
     </section>
     <h2 class="text-2xl font-medium  uppercase">Your Bookings</h2>
   </main>
