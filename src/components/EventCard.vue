25f5e7a1b0298cfc042e674cd467a4fca0af98e3
Nimira43 <n.rabone72@aol.com>
2025-02-23 02:16:59 +0000

    Added a "Register" button that emits a register event when clicked

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,7 +4,10 @@
     <p class="p-4 text-dark font-normal uppercase">{{ date }}</p>
     <p class="p-4 text-dark font-normal uppercase">{{ description }}</p>
     <section class="flex justify-end p-4">
-      <button class="bg-prime text-light py-2 px-3 rounded-md hover:bg-prime-light cursor-pointer text-sm uppercase">
+      <button 
+        class="bg-prime text-light py-2 px-3 rounded-md hover:bg-prime-light cursor-pointer text-sm uppercase"
+        @click="$emit('register')"
+      >
         Register
       </button>
 
